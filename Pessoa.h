571c71e0fb3a0feeaf650da12e34c31c492c1bbd
Raagranato <raagranato@gmail.com>
2026-03-04 15:36:59 -0300

    Add files via upload

---
+++ Pessoa.h
@@ -0,0 +1,40 @@
+#ifndef PESSOA_H
+#define PESSOA_H
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Pais;
+class Modalidade;
+
+class Pessoa {
+private:
+    Pais* pais;
+    vector<Modalidade*> modalidades;
+public:
+    string nome;
+    string genero;
+    int anoNascimento;
+
+    // Construtor
+    Pessoa( Pais* pais, string nome, string genero, int anoNascimento);
+
+    // Destrutor
+    virtual ~Pessoa();
+
+    // Getters
+    Pais* getPais();
+    string getNome();
+    vector<Modalidade*> getModalidade();
+
+    // Setters
+    void setPais(Pais* p);
+    void setModalidade(Modalidade* m);
+
+    // (Polimorfismo) - Metodo virtual que exibe os dados comuns de Pessoa, servindo de base para as classes derivadas
+    virtual void exibirDados();
+};
+#endif
---
+++ README.md
@@ -0,0 +1,12 @@
+
+Compilação:
+g++ Atleta.cpp Medalha.cpp Modalidade.cpp Pais.cpp Pessoa.cpp Treinador.cpp Main.cpp -o exe -Wall
+
+Compilação com a interface gráfica:
+g++ Atleta.cpp Medalha.cpp Modalidade.cpp Pais.cpp Pessoa.cpp Treinador.cpp Maingraf.cpp -o exe -Wall
+
+Executar:
+./exe
+
+Link do vídeo:
+https://drive.google.com/drive/u/0/folders/1t8tqIowF8mHIV7J7aPGehK60WmxbBJtA
